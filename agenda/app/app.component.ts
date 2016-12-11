5d92255ecbe62849d4e6fcc5aaa3c41a6d5f69f6
José Sánchez <joseynene@hotmail.com>
2016-12-11 19:54:45 +0100

    Pequennas modificaciones

--- agenda/app/app.component.ts
+++ agenda/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   // En 'selector' se indica el elemento HTML en el cuál se instanciará el componente
